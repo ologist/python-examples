d4fb394bc7d5cdff1a0bbaff4b24ed842414929d
josh <ologist0@gmail.com>
2013-07-28 12:54:32 +0900

    package refactoring

--- infra.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/test'
-app.debug = True
-
-db = SQLAlchemy(app)
